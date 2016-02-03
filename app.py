d7ab751d2aa9a829d331763351c63fd7eaf35474
Alaf Azam <alafazam@gmail.com>
2016-02-04 00:01:52 +0530

    simple pearson

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from recommendations import critics
-from sim_dist import sim_distance
+from sim_dist import *
 
 
 
 t = sim_distance(critics,'Lisa Rose','Gene Seymour')
+q = sim_pearson(critics,'Lisa Rose','Gene Seymour')
+
+print "distance:"
 print t
 
+print "preason coeff"
+print q
--- sim_dist.py
+++ sim_dist.py
@@ -16,3 +16,45 @@ def sim_distance(prefs, person1, person2):
 		if item in prefs[person2]
 	])
 	return 1/(1 + sum_of_squares)
+
+
+# Returns the Pearson correlation coefficient for p1 and p2
+
+
+def sim_pearson(prefs,p1,p2):
+	# Get the list of mutually rated items
+	si={}
+	for item in prefs[p1]:
+		if item in prefs[p2]: si[item]=1
+	
+	# Find the number of elements
+	n=len(si)
+	
+	# if they are no ratings in common, return 0
+	if n==0: return 0
+	
+	# Add up all the preferences
+	sum1=sum([prefs[p1][it] for it in si])
+	sum2=sum([prefs[p2][it] for it in si])
+	
+	# Sum up the squares
+	sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
+	sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
+	
+	# Sum up the products
+	pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+	
+	# Calculate Pearson score
+	num=pSum-(sum1*sum2/n)
+	den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+	if den==0: return 0
+	
+	r=num/den
+	
+	return r
+
+
+
+
+
+
