c3781f473af3dad12d5789162ac0798ce58e87b2
Alaf Azam <alafazam@gmail.com>
2016-02-06 00:53:25 +0530

    matching products

--- app.py
+++ app.py
@@ -11,11 +11,23 @@ print t
 
 print "preason coeff"
 print q
-
+print ""
 
 scores = topMatches(critics, 'Toby', n=3)
 print scores
+print ""
 
 
 recommendation = getRecommendations(critics,'Toby')
 print recommendation
+print ""
+
+
+movies = transformPrefs(critics)
+print movies
+print ""
+
+
+scores2 = topMatches(movies,'Superman Returns')
+print scores2
+print ""
--- sim_dist.py
+++ sim_dist.py
@@ -95,3 +95,13 @@ def getRecommendations(prefs,person,similarity=sim_pearson):
 	rankings.sort()
 	rankings.reverse()
 	return rankings
+
+
+def transformPrefs(prefs):
+	result={}
+	for person in prefs:
+		for item in prefs[person]:
+			result.setdefault(item,{})
+		# Flip item and person
+		result[item][person]=prefs[person][item]
+	return result
