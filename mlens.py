3023ff59e9b73c1c1b765c01235e9bf532b5ae28
Alaf Azam <alafazam@gmail.com>
2016-02-08 00:29:41 +0530

    working with movie lens data set

--- mlens.py
+++ mlens.py
@@ -11,4 +11,13 @@ print ds['100']
 
 
 recommendation_for_user = getRecommendations(ds,'100')[0:30]
-print "recommendation for user :\n", recommendation_for_user
+print "recommendation user based:\n", recommendation_for_user
+
+
+
+
+itemsim = calculateSimilarItems(ds,n=50)
+
+
+recommend_from_items = getRecommendedItems(ds,itemsim,'100')[0:30]
+print "recommend items based:\n", recommendation_for_user
