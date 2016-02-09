e99f1ecd9d183e4f1e7097f50440220e1e681953
Alaf Azam <alafazam@gmail.com>
2016-02-10 01:01:00 +0530

    hirarchiral clustering

---
+++ cluster.py
@@ -0,0 +1,7 @@
+class bicluster:
+	def __init__(self, vec, left=None, right=None, distance=0, id=None):
+		self.left = left
+		self.right = right
+		self.vec = vec
+		self.id = id
+		self.distance = distance
--- clusters.py
+++ clusters.py
@@ -1,3 +1,7 @@
+from pearson_corelation2 import *
+from cluster import *
+
+
 def readfile(filename):
 	lines = [line for line in file(filename)]
 	#First line is column titles
@@ -11,3 +15,44 @@ def readfile(filename):
 		# the data for this row is remaineder of the row
 		data.append([float(x) for x in p[1:]])
 	return rownames,colnames,data
+
+
+
+
+def hcluster(rows, distance = pearson):
+	distances =  {}
+	clurrentClustid = -1
+
+	#clusters are initially just the row
+	clust = [bicluster(rows[i],id=i) for i in range(len(rows))]
+
+	while len(clust)>1:
+		lowestpair = (0,1)
+		closest = distance(clust[0].vec,clust[1].vec)
+
+		#loop through every pair looking for smallest distance
+		for i in range(len(clust)):
+			for j in range(i+1, len(clust)):
+				# distance is the cache of distance calulations
+				if(clust[i].id clust[j].id) not in distances:
+					distances[(clust[i].id,clust[j].id)] = distance(clust[i].vec,clust[j].vec)
+
+				d = distances[(clust[i].id,clust[j].id)]
+
+				if d < closest:
+					closest = d
+					lowestpair = (i,j)
+		# calulate the average of two clusters
+
+		mergevec = [(clust[lowestpair[0]].vec + clust[lowestpair[1]].vec)/2.0
+		for in range(len(clust[0].vec))]
+
+		#create the new cluster
+		newcluster=bicluster(mergevec,left=clust[lowestpair[0]], right=clust[lowestpair[1]],distance=closest,id=currentclustid)
+		# cluster ids that weren't in the original set are negative
+		currentclustid-=1
+		del clust[lowestpair[1]]
+		del clust[lowestpair[0]]
+		clust.append(newcluster)
+
+	return clust[0]
