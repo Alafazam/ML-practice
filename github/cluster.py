9adb6c5e00812d9e3b73b5b85256ee2f6261e551
Alaf Azam <alafazam@gmail.com>
2016-02-23 00:39:58 +0530

    genrate image

---
+++ github/cluster.py
@@ -0,0 +1,8 @@
+class bicluster:
+	def __init__(self, vec, left=None, right=None, distance=0, id=None):
+		self.left = left
+		self.right = right
+		self.vec = vec
+		self.id = id
+		self.distance = distance
+	
