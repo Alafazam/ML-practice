33d597e53f91cabe2db27c51de3a5a7d3f360238
Alaf Azam <alafazam@gmail.com>
2016-03-29 23:45:53 +0530

    training data

--- nn.py
+++ nn.py
@@ -12,27 +12,13 @@ target = data.target
 target_names = data.target_names
 labels = target_names[target]
 
-# training = [x for x in range(0,150)]
-# training.shuffle(randomz)
-# training = randomz[:10]
-# training = np.array(training)
-
-# training_features = features[training]
-# training_labels = labels[training]
-# training_target = target[training]
-
-# features = features[~training]
-# labels = labels[~training]
-# target = target[~training]
-
-
 
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cross_validation import KFold
 
 
-classifier = KNeighborsClassifier(n_neighbors=1)
+classifier = KNeighborsClassifier(n_neighbors=3)
 
 kf = KFold(len(features), n_folds=5, shuffle=True)
 # `means` will be a list of mean accuracies (one entry per fold)
