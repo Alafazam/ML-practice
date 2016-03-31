c2a6b515a9b14119ba20618b157cdcba8ac4d5ac
Alaf Azam <alafazam@gmail.com>
2016-03-31 23:44:43 +0530

    crawler 1

---
+++ PCI chapter 4 Search engine/run.py
@@ -0,0 +1,4 @@
+import searchengine
+pagelist=['https://en.wikipedia.org/wiki/Python']
+crawler=searchengine.crawler('')
+crawler.crawl(pagelist)
--- PCI chapter 4 Search engine/searchengine.py
+++ PCI chapter 4 Search engine/searchengine.py
@@ -4,11 +4,13 @@ from urlparse import urljoin
 # Create a list of words to ignore
 ignorewords=set(['the','of','to','and','a','in','is','it'])
 
+
 class crawler:
   # Initialize the crawler with the name of database
-  def __init_ _(self,dbname):
+  def __init__(self,dbname):
+    self.listOfUrls = []
     pass
-  def __del_ _(self):
+  def __del__(self):
     pass
   def dbcommit(self):
     pass
@@ -22,6 +24,7 @@ class crawler:
   # Index an individual page
   def addtoindex(self,url,soup):
     print 'Indexing %s' % url
+    self.listOfUrls.append(url)
 
   # Extract the text from an HTML page (no tags)
   def gettextonly(self,soup):
@@ -33,19 +36,50 @@ class crawler:
 
   # Return true if this url is already indexed
   def isindexed(self,url):
-    return False
+    return url in self.listOfUrls
+    # return False
 
   # Add a link between two pages
   def addlinkref(self,urlFrom,urlTo,linkText):
     pass
 
 
+  def createindextables(self):
+    pass
+
   # Starting with a list of pages, do a breadth
   # first search to the given depth, indexing pages
   # as we go
-  def crawl(self,pages,depth=2):
-    pass
-
   # Create the database tables
-  def createindextables(self):
-    pass
+  def crawl(self,pages,depth=2):
+    for i in range(depth):
+      newpages={}
+      for page in pages:
+        try:
+          c=urllib2.urlopen(page)
+        except:
+          print "Could not open %s" % page
+          continue
+        try:
+          soup=BeautifulSoup(c.read())
+          self.addtoindex(page,soup)
+
+          links=soup('a')
+          for link in links:
+            if ('href' in dict(link.attrs)):
+              print 'processing linl %s' % link
+              url=urljoin(page,link['href'])
+              if url.find("'")!=-1: continue
+              url=url.split('#')[0]  # remove location portion
+              if url[0:4]=='http' and not self.isindexed(url):
+                newpages[url]=1
+              linkText=self.gettextonly(link)
+              self.addlinkref(page,url,linkText)
+
+          self.dbcommit()
+        except:
+          print "Could not parse page %s" % page
+
+      pages=newpages
+
+
