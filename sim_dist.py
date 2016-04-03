0933f5f49959b908d1ff14c8b58f62e983632b4f
Alaf Azam <alafazam@gmail.com>
2016-04-03 18:51:28 +0530

    remove extra whitespaces

--- sim_dist.py
+++ sim_dist.py
@@ -8,14 +8,14 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 # Returns a distance - based similarity score for person1 and person2
-def sim_distance(prefs, person1, person2): 
+def sim_distance(prefs, person1, person2):
 	#Get the list of shared_items
 	si = {}
 	for item in prefs[person1]:
 		if item in prefs[person2]:
 			si[item] = 1	#if they have no ratings in common,
 
-	if len(si) == 0: 
+	if len(si) == 0:
 		return 0	# Add up the squares of all the differences
 	sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item], 2)
 		for item in prefs[person1]
@@ -30,31 +30,31 @@ def sim_pearson(prefs,p1,p2):
 	si={}
 	for item in prefs[p1]:
 		if item in prefs[p2]: si[item]=1
-	
+
 	# Find the number of elements
 	n=len(si)
-	
+
 	# if they are no ratings in common, return 0
 	if n==0: return 0
-	
+
 	# Add up all the preferences
 	sum1=sum([prefs[p1][it] for it in si])
 	sum2=sum([prefs[p2][it] for it in si])
-	
+
 	# Sum up the squares
 	sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
 	sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
-	
+
 	# Sum up the products
 	pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
-	
+
 	# Calculate Pearson score
 	num=pSum-(sum1*sum2/n)
 	den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
 	if den==0: return 0
-	
+
 	r=num/den
-	
+
 	return r
 
 
@@ -78,11 +78,11 @@ def getRecommendations(prefs,person,similarity=sim_pearson):
 		# don't compare me to myself
 		if other==person: continue
 		sim=similarity(prefs,person,other)
-		
+
 		# ignore scores of zero or lower
 		if sim<=0: continue
 		for item in prefs[other]:
-			
+
 			# only score movies I haven't seen yet
 			if item not in prefs[person] or prefs[person][item]==0:
 				# Similarity * Score
@@ -91,7 +91,7 @@ def getRecommendations(prefs,person,similarity=sim_pearson):
 				# Sum of similarities
 				simSums.setdefault(item,0)
 				simSums[item]+=sim
-	
+
 	# Create the normalized list
 	rankings=[(total/simSums[item],item) for item,total in totals.items( )]
 	# Return the sorted list
@@ -115,7 +115,7 @@ def calculateSimilarItems(prefs,n=10):
 	# Create a dictionary of items showing which other items they
 	# are most similar to.
 	result={}
-	
+
 	# Invert the preference matrix to be item-centric
 	itemPrefs=transformPrefs(prefs)
 	c=0
@@ -136,24 +136,24 @@ def getRecommendedItems(prefs,itemMatch,user):
 
 	# Loop over items rated by this user
 	for (item,rating) in userRatings.items( ):
-	
+
 		# Loop over items similar to this one
 		for (similarity,item2) in itemMatch[item]:
-	
+
 			# Ignore if this user has already rated this item
 			if item2 in userRatings: continue
-	
+
 			# Weighted sum of rating times similarity
 			scores.setdefault(item2,0)
 			scores[item2]+=similarity*rating
-	
+
 			# Sum of all the similarities
 			totalSim.setdefault(item2,0)
 			totalSim[item2]+=similarity
-	
+
 	# Divide each total score by total weighting to get an average
 	rankings=[(score/totalSim[item],item) for item,score in scores.items( )]
-	
+
 	# Return the rankings from highest to lowest
 	rankings.sort( )
 	rankings.reverse( )
@@ -163,20 +163,20 @@ def getRecommendedItems(prefs,itemMatch,user):
 
 
 def loadMovieLens(path='/dataset/ml-100k'):
-	
+
 	# Get movie titles
 	movies={}
-	
+
 	for line in open(_basedir + path+'/u.item'):
 		(id,title)=line.split('|')[0:2]
 		movies[id]=title
-	
+
 	# Load data
 	prefs={}
-	
+
 	for line in open(_basedir + path+'/u.data'):
 		(user,movieid,rating,ts)=line.split('\t')
 		prefs.setdefault(user,{})
 		prefs[user][movies[movieid]]=float(rating)
-	
+
 	return prefs
